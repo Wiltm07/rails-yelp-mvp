1c8802c23fbc35a569d70f11a826f32d2bc58ae0
Wilber Tuberquia Madrid <wilbertuberquia234621@correo.itm.edu.co>
2022-09-15 19:31:35 -0500

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.find_each(&:destroy)
+Restaurant.create(name: "Bigotes", address: "Cr 55 # 50A-45", category: "italian")
+Restaurant.create(name: "Juancho", address: "Cr 55 # 60A-45", category: "french")
+Restaurant.create(name: "Nutibara", address: "Cr 65 # 50A-45", category: "chinese")
+Restaurant.create(name: "Alberto", address: "Cr 65 # 80A-45", category: "italian")
+Restaurant.create(name: "Guadalupe", address: "Cr 75 # 60A-55", category: "japanese")
+
+puts "inserted register in the databse"
